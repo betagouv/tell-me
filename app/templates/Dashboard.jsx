42cc481effd4e32c95b256ee67f1cb5874909066
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-10-25 15:15:34 +0200

    style(app): normalize Dashboard

---
+++ app/templates/Dashboard.jsx
@@ -0,0 +1,13 @@
+import AdminBox from '../atoms/AdminBox'
+import AdminHeader from '../atoms/AdminHeader'
+import Title from '../atoms/Title'
+
+export default function Dashboard() {
+  return (
+    <AdminBox>
+      <AdminHeader>
+        <Title>Dashboard</Title>
+      </AdminHeader>
+    </AdminBox>
+  )
+}
