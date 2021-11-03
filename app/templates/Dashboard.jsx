6be96e6fc3f9b056ce2ca50c15fb89610fdc84fb
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-03 15:33:59 +0100

    feat(app): update Dashboard locales

--- app/templates/Dashboard.jsx
+++ app/templates/Dashboard.jsx
@@ -1,12 +1,22 @@
+import { useIntl } from 'react-intl'
+
 import AdminBox from '../atoms/AdminBox'
 import AdminHeader from '../atoms/AdminHeader'
 import Title from '../atoms/Title'
 
 export default function Dashboard() {
+  const intl = useIntl()
+
   return (
     <AdminBox>
       <AdminHeader>
-        <Title>Dashboard</Title>
+        <Title>
+          {intl.formatMessage({
+            defaultMessage: 'Dashboard',
+            description: '[Dashboard] Title.',
+            id: '/2pv6j',
+          })}
+        </Title>
       </AdminHeader>
     </AdminBox>
   )
