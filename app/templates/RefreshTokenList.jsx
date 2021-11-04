e1cd3ea6b2e665e6e210e4be01c6f27f451b352f
Ivan Gabriele <ivan.gabriele@gmail.com>
2021-11-04 14:51:31 +0100

    feat(app): update RefreshTokenList locales

--- app/templates/RefreshTokenList.jsx
+++ app/templates/RefreshTokenList.jsx
@@ -1,6 +1,7 @@
 import { Card, Table } from '@singularity-ui/core'
 import { useEffect, useState } from 'react'
 import { Trash } from 'react-feather'
+import { useIntl } from 'react-intl'
 
 import AdminBox from '../atoms/AdminBox'
 import AdminHeader from '../atoms/AdminHeader'
@@ -11,6 +12,7 @@ import useIsMounted from '../hooks/useIsMounted'
 export default function RefreshTokenList() {
   const [isLoading, setIsLoading] = useState(true)
   const [refreshTokens, setRefreshTokens] = useState([])
+  const intl = useIntl()
   const api = useApi()
   const isMounted = useIsMounted()
 
@@ -40,23 +42,39 @@ export default function RefreshTokenList() {
     {
       isSortable: true,
       key: 'user.email',
-      label: 'Email',
+      label: intl.formatMessage({
+        defaultMessage: 'Email',
+        description: '[Refresh Tokens List] Table "email" column label.',
+        id: 'jGB2t1',
+      }),
     },
     {
       isSortable: true,
       key: 'ip',
-      label: 'IP',
+      label: intl.formatMessage({
+        defaultMessage: 'IP',
+        description: '[Refresh Tokens List] Table "IP" column label.',
+        id: '20hT19',
+      }),
     },
     {
       isSortable: true,
       key: 'ttl',
-      label: 'TTL',
+      label: intl.formatMessage({
+        defaultMessage: 'TTL',
+        description: '[Refresh Tokens List] Table "TTL" column label.',
+        id: '+lem9Q',
+      }),
     },
     {
       accent: 'danger',
       action: deleteOneTimeToken,
       Icon: Trash,
-      label: 'Delete this refresh token',
+      label: intl.formatMessage({
+        defaultMessage: 'Delete this refresh token',
+        description: '[Refresh Tokens List] Table row "token deletion" button label.',
+        id: 'uaDzBk',
+      }),
       type: 'action',
     },
   ]
@@ -64,7 +82,13 @@ export default function RefreshTokenList() {
   return (
     <AdminBox>
       <AdminHeader>
-        <Title>Refresh Tokens</Title>
+        <Title>
+          {intl.formatMessage({
+            defaultMessage: 'Refresh Tokens',
+            description: '[Refresh Tokens List] Page title.',
+            id: 'Ikt38A',
+          })}
+        </Title>
       </AdminHeader>
 
       <Card>
