ef986f1df1301f4b3d9bf731f13c5b9d2570cee5
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-06-03 16:45:46 +0200

    fix(docker) run prebuild as prestart when deployed via Dockerfile

--- scripts/build/prestart.sh
+++ scripts/build/prestart.sh
@@ -6,7 +6,7 @@ set -e
 # Load .env file
 export $(egrep -v '^(#|RSA_PRIVATE_KEY|NEXT_PUBLIC_RSA_PUBLIC_KEY)' ./.env | xargs) > /dev/null
 
-if [ "${IS_DOCKER}" == "true" ]; then
+if [ "${IS_DOCKER}" != "true" ]; then
   exit 0
 fi
 
